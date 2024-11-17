aff877e10317796ca20219b69f799116ba43b4f0
azlanbaig <azlanbaigbaig@gmail.com>
2024-11-17 21:25:27 +0500

    Add project files.

---
+++ HelloWorld/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    public class Customer
+    {
+
+        public string Name;
+        public int Id;
+        public List<Order> Orders;
+
+        public Customer()
+        {
+            
+        }
+
+
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Person
+    {
+        public string Name;
+        public Person(string name)
+        {
+        this.Name = name;
+
+            Console.WriteLine("Hello My name is {0}", name);
+        }
+        //public void Introduce(string to)
+        //{
+        //    Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        //}
+
+
+        //public static Person Parse(string str)
+        //{
+        //    var person = new Person();
+        //    person.Name = str;
+
+        //    return person;
+        //}
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var number = 3;
+            //var float_value = 3.5f;
+            //var character = 'a';
+            //var string_value = "This is a string";
+            //Console.WriteLine(number);
+            //Console.WriteLine(float_value);
+            //Console.WriteLine(character);
+            //Console.WriteLine(string_value);
+            //const float Pi = 3.142f;
+            //Console.WriteLine(Pi);
+            //person.Introduce("Azlan");
+
+            var person = new Person("John");
+
+
+        }
+    }
+}
